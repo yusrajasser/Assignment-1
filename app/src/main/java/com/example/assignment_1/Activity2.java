9e84d14e929f8c5877589e57772b4481eec076bc
yusrajasser <yusra.jasser96@gmail.com>
2021-04-07 23:27:13 +0300

    version 1

---
+++ app/src/main/java/com/example/assignment_1/Activity2.java
@@ -0,0 +1,4 @@
+package com.example.assignment_1;
+
+public class Activity2 {
+}
--- app/src/main/java/com/example/assignment_1/MainActivity.java
+++ app/src/main/java/com/example/assignment_1/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.assignment_1;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
